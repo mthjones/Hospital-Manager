8af8d3250a78c394190954c07d38be2e1bf2b441
Matt Jones <m.t.h.jones@gmail.com>
2012-01-23 18:41:12 -0700

    Testing setup

---
+++ src/testTest.java
@@ -0,0 +1,9 @@
+public class HelloWorldTest extends junit.framework.TestCase
+{
+	public void testNothing() {
+    }
+    
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+}
