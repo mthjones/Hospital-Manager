4158792f51d5594bbe6c05785f6bcc381eee64d0
Andrew Midwinter <akmidwinter@gmail.com>
2012-02-19 14:32:20 -0700

    Added stuff.

--- src/hms/HMSCommands/CreatePatientCommand.java
+++ src/hms/HMSCommands/CreatePatientCommand.java
@@ -2,11 +2,36 @@ package hms.HMSCommands;
 
 public class CreatePatientCommand
 {
-	public CreatePatientCommand()
-	{
+	//private Patient patient;
 	
-//	public run(Patient patient)
-//	{
-//	}
+	/*
+	 * Creates new object of CreatePatientCommand
+	 * 
+	 * @param {@link Patient} p : the object that store the information about the patient being created
+	 * 
+	 */
+	/*public CreatePatientCommand(Patient p)
+	{
+		patient = p;
+	}*/
+	
+	/*
+	 * Runs the CreatePatientCommand, adding the patient to the database
+	 * 
+	 * @return The result of the creation.
+	 * 
+	 */
+	public boolean run()
+	{	
+		//connect to db
+		
+		//Generate query
+		
+		//Send query to db
+		
+		//Receive result of query from db
+		
+		return true;
+	}
 
 }
