b21b654d13d1cfcf507ccb4744f0c41bef37c1bf
Mark Mullen <markmullen9@hotmail.com>
2012-02-19 15:13:07 -0700

    Added some comments to clarify functionality of EditPatientCommand

--- src/hms/HMSCommands/EditPatientCommand.java
+++ src/hms/HMSCommands/EditPatientCommand.java
@@ -2,12 +2,35 @@ package hms.HMSCommands;
 
 public class EditPatientCommand
 {
-	public EditPatientCommand()
+	//private Patient patient;
+	
+	/*
+	public EditPatientCommand(Patient patient)
 	{
 	}
+	*/
 	
-//	public run(Patient patient)
-//	{
-//	}
+	
+	/*
+	 * Runs the EditPatientCommand, replacing the patient with the same pID in the database
+	 * 
+	 * @return The result of the creation.
+	 * 
+	 */
+	public boolean run()
+	{
+		//Use the private patient variable
+		
+		//Replace the corresponding patient via matching patient ID's in the database
+		
+		//If pID does not exist, send error message, do not insert in database
+		
+		//Return true for success, false for failure
+		
+		//Order of operations: Query database for matching pID, replace that entry with the private variable
+		//Check for successful insertion, return true or false depending on whether the insert worked or not
+		
+		return true;
+	}
 
 }
