2e02c0354c2970bba1be18efdba19ff7b62f0537
Katya-g <31415926e>
2012-02-19 18:09:03 -0700

    + added Save and Close button

--- src/hms/Views/PatientView.java
+++ src/hms/Views/PatientView.java
@@ -3,10 +3,19 @@ package hms.Views;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PatientView {
 
 	public JFrame frmPatient;
+	private JTextField textFieldTelephoneNumber;
 
 	/**
 	 * Create the application.
@@ -23,6 +32,52 @@ public class PatientView {
 		frmPatient.setTitle("Patient");
 		frmPatient.setBounds(100, 100, 541, 425);
 		frmPatient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblName = new JLabel("Name");
+		
+		JLabel lblTelephoneNumber = new JLabel("Telephone Number");
+		
+		textFieldTelephoneNumber = new JTextField();
+		textFieldTelephoneNumber.setColumns(10);
+		
+		JButton btnSaveAndClose = new JButton("Save and Close");
+		btnSaveAndClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmPatient.dispose();
+			}
+		});
+		GroupLayout groupLayout = new GroupLayout(frmPatient.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(22)
+							.addComponent(lblTelephoneNumber, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(textFieldTelephoneNumber, GroupLayout.PREFERRED_SIZE, 168, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(82)
+							.addComponent(lblName)))
+					.addContainerGap(235, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap(426, Short.MAX_VALUE)
+					.addComponent(btnSaveAndClose)
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(26)
+					.addComponent(lblName)
+					.addGap(26)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textFieldTelephoneNumber, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblTelephoneNumber))
+					.addPreferredGap(ComponentPlacement.RELATED, 267, Short.MAX_VALUE)
+					.addComponent(btnSaveAndClose)
+					.addContainerGap())
+		);
+		frmPatient.getContentPane().setLayout(groupLayout);
 	}
-
 }
