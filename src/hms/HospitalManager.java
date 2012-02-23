e608a1eaa94961e28566dd496040b9c1e9cd52f7
Matt Jones <m.t.h.jones@gmail.com>
2012-02-23 16:32:31 -0700

    Updated main class to create a new user on start

--- src/hms/HospitalManager.java
+++ src/hms/HospitalManager.java
@@ -1,15 +1,18 @@
 package hms;
 
 import hms.Views.LoginView;
+import hms.models.User;
 
 class HospitalManager
 {
 	public static void main(String[] args) {
 		try {
+			User new_user = new User("admin", "password");
+			new_user.create();
 	 		LoginView loginView = new LoginView();
 			loginView.frmLogin.setVisible(true);
 	 	} catch (Exception e) {
-	 		e.printStackTrace();
+	 		
 	 	}
 	}
 }
