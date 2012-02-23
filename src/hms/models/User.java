53d3b13f6b308800dfd86ec439db2a945ba48cd7
Matt Jones <m.t.h.jones@gmail.com>
2012-02-22 18:18:48 -0700

    Updated User to be functional

--- src/hms/models/User.java
+++ src/hms/models/User.java
@@ -1,12 +1,19 @@
 package hms.models;
 
+import java.util.ArrayList;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 import hms.db.Database;
 import hms.Managers.Encryptor;
 
 public class User {
+	private String username;
+	private String password;
+	public ArrayList<String> errors = new ArrayList<String>();
+	
 	/**
 	 * Authenticates the given user credentials against the user table in the database.
 	 * If the username and password match the data in the table, returns true. Otherwise 
@@ -16,21 +23,85 @@ public class User {
 	 * @return true if the credentials match; false otherwise
 	 */
 	public static boolean authenticate(String username, String password) throws SQLException {
-		ResultSet user = Database.getInstance().executeQuery("SELECT username, password FROM user WHERE username = '" + username + "'");
-		if (user.getString("password") == Encryptor.encode(password)) {
+		ResultSet user = Database.getInstance().executeQuery("SELECT username, password, COUNT(*) FROM user WHERE username = '" + username + "'");
+		user.next();
+		if (user.getInt(3) == 0) {
+			return false;
+		}
+		if (user.getString("password").equals(DigestUtils.md5Hex(password))) {
 			return true;
 		}
 		return false;
 	}
 	
-	public static User create(String username, String password) throws SQLException {
-		ResultSet user = Database.getInstance().executeQuery("SELECT COUNT(*) FROM user WHERE username = '" + username + "'");
+	/**
+	 * Tries to save the user to the database. If the username already exists in the database,
+	 * it returns false. If the user is saved successfully, then it returns true.
+	 * @return true if the user is saved succesfully; false otherwise
+	 */
+	public boolean save() throws SQLException {
+		ResultSet user = Database.getInstance().executeQuery("SELECT COUNT(*) FROM user WHERE username = '" + this.username + "'");
 		user.next();
 		int count = user.getInt(1);
 		if (count > 0) {
-			return null;
+			this.errors.add("Username exists");
+			return false;
 		} else {
-			return null;
+			try {
+				ResultSet users = Database.getInstance().executeQuery("SELECT * FROM user");
+				users.moveToInsertRow();
+				users.updateString("username", this.username);
+				users.updateString("password", this.password);
+				users.insertRow();
+				this.errors.clear();
+				return true;
+			} catch (SQLException sqle) {
+				System.err.println(sqle);
+				this.errors.add("Couldn't add to database");
+				return false;
+			}
 		}
+	}
+	
+	/**
+	 * Tries to delete the user from the database. If the username already exists in the database,
+	 * it returns false. If the user is saved successfully, then it returns true.
+	 * @return true if the user is saved succesfully; false otherwise
+	 */
+	public boolean delete() throws SQLException {
+		try {
+			int users = Database.getInstance().executeUpdate("DELETE FROM user WHERE username = '" + this.username + "'");
+			if (users == 0) {
+				this.errors.add("User does not exist");
+				return false;
+			}
+			this.errors.clear();
+			return true;
+		} catch (SQLException sqle) {
+			System.err.println(sqle);
+			this.errors.add("Could not delete user");
+			return false;
+		}
+	}
+	
+	protected User(String username, String password) {
+		this.username = username;
+		this.password = DigestUtils.md5Hex(password);
+	}
+	
+	public void setPassword(String password) {
+		this.password = DigestUtils.md5Hex(password);
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String toString() {
+		return "<User: @username=" + this.username + "; @password=" + this.password + ">";
 	}
 }
--- test/hms/models/UserTest.java
+++ test/hms/models/UserTest.java
@@ -1,5 +1,8 @@
 package hms.models;
 
+import java.sql.SQLException;
+import java.util.Arrays;
+
 import hms.models.User;
 
 import org.junit.*;
@@ -7,7 +10,27 @@ import static org.junit.Assert.*;
 
 public class UserTest {
 	@Test 
-	public void test_nothing() {
-		assertTrue(true);
+	public void test_createUser() throws SQLException {
+		User user = new User("testuser", "testpass");
+		assertTrue(user.save());
+		user.delete();
+	}
+	
+	@Test
+	public void test_deleteUser() throws SQLException {
+		User user = new User("testuser", "testpass");
+		user.save();
+		assertTrue(user.delete());
+	}
+	
+	@Test
+	public void test_authenticateUser() throws SQLException {
+		User user = new User("testuser2", "testpass");
+		user.save();
+		assertTrue("right user, right pass", User.authenticate("testuser2", "testpass"));
+		assertFalse("wrong user, right pass", User.authenticate("wronguser", "testpass"));
+		assertFalse("right user, wrong pass", User.authenticate("testuser2", "wrongpass"));
+		assertFalse("wrong user, wrong pass", User.authenticate("wronguser", "wrongpass"));
+		user.delete();
 	}
 }
