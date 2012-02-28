b386d56b998cca04b0cea5525ce0c579c8f4a61a
Jamie <jhbertra@gmail.com>
2012-02-27 18:15:39 -0700

    added test scripts (comments)

--- test/hms/integrationIteration1/IntegrationTest.java
+++ test/hms/integrationIteration1/IntegrationTest.java
@@ -13,6 +13,13 @@ import org.junit.Test;
 public class IntegrationTest {
 	
 	//tests for login system.  These tests were performed manually
+    
+    /**
+     * Attempt to login by entering a valid username and password 
+     * at the login screen.
+     *
+     * expected: patient screen should display.
+     */
 	@Test
 	public void testLogin_UserInDatabase() {
 		boolean testPerformed = true;
@@ -20,6 +27,12 @@ public class IntegrationTest {
 		assertTrue(testPerformed && testPassed);
 	}
 
+    /**
+     * Attempt to login by entering an invalid username in the
+     * login dialog.
+     *
+     * expected: invalid login prompt displayed
+     */
 	@Test
 	public void testLogin_UserNotInDatabase() {
 		boolean testPerformed = true;
@@ -27,13 +40,25 @@ public class IntegrationTest {
 		assertTrue(testPerformed && testPassed);	
 	}
 
+    /**
+     * Attempt to login by entering a valid username and invalid
+     * password in the login dialog.
+     *
+     * expected: invalid login prompt displayed
+     */
 	@Test
 	public void testLogin_IncorrectPassword() {
 		boolean testPerformed = true;
 		boolean testPassed = true;
 		assertTrue(testPerformed && testPassed);
 	}
-
+    
+    /**
+     * Attempt to login by entering a valid username and null
+     * password in the login dialog.
+     *
+     * expected: invalid login prompt displayed
+     */
 	@Test
 	public void testLogin_NullPassword() {
 		boolean testPerformed = true;
@@ -57,6 +82,11 @@ public class IntegrationTest {
 	
 	//tests for viewing and editing patients - tests are manual
 	
+    /**
+     * open the patient list viewer
+     *
+     * expected: all patients in db displayed
+     */
 	@Test
 	public void testViewPatient_AllPatientsDisplayed() {
 		boolean testPerformed = false;
@@ -64,6 +94,11 @@ public class IntegrationTest {
 		assertTrue(testPerformed && testPassed);
 	}
 	
+    /**
+     * add a new patient, refresh the viewer
+     *
+     * expected: all patients in db displayed, including new one
+     */
 	@Test
 	public void testViewPatient_ListRefreshesWhenNewPatientsAdded() {
 		boolean testPerformed = true;
@@ -71,6 +106,11 @@ public class IntegrationTest {
 		assertTrue(testPerformed && testPassed);
 	}
 	
+    /**
+     * alter a patient and refresh the viewer
+     *
+     * expected: all patients in db displayed, including changes made
+     */
 	@Test
 	public void testViewPatient_ListRefreshesWhenPatientIsEditied() {
 		boolean testPerformed = true;
