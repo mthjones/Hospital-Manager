9113b9833cc95bae42e61601e0b03409ea3ce835
Andrew Midwinter <akmidwinter@gmail.com>
2012-03-13 10:42:55 -0600

    Added more tests.

--- test/hms/models/NurseTest.java
+++ test/hms/models/NurseTest.java
@@ -43,5 +43,37 @@ public class NurseTest {
 		assertFalse(nurse2.create());
 		nurse1.delete();
 	}
+	
+	@Test
+	public void test_deleteNurse() {
+		try {
+			Nurse nurse = new Nurse("nurse1", "555-555-5555", "123-456-7890", "Jane@nurses.com", "123, 4th avenue", "123-456-789", 1111, "f", 50000);
+			nurse.create();
+			assertTrue(nurse.delete());
+		} catch(SQLException e) {
+			fail("SQLException occurred.");
+		}
+	}
+	
+	@Test
+	public void test_cannotDeleteNonexistantNurse(){
+		try{
+			Nurse nurse = new Nurse("Jane Doe", "123-456-7890", "555-5555", "test@test.com", "123, 4th avenue", "123-456-7890", 2222, "f", 100000);
+			assertFalse(nurse.delete());
+		} catch(SQLException e) {
+			fail("SQLException occured.");
+		}
+	}
+	
+	@Test
+	public void test_findNurse() {
+		try{
+			Nurse nurse = new Nurse("Jane Doe", "123-456-7890", "555-5555", "test@test.com", "123, 4th avenue", "123-456-7890", 2222, "f", 100000);
+			nurse.create();
+			assertNotNull(Nurse.find(2222));
+		} catch(SQLException e) {
+			fail("SQLException occurred.");
+		}
+	}
 
 }
