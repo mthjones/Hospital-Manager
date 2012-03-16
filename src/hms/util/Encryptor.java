e0615c8e3d0622e70d981e15fc4366b64c24839f
Thomas Frampton <tbframpt@ucalgary.ca>
2012-03-16 10:49:04 -0600

    updated encryptor

--- src/hms/util/Encryptor.java
+++ src/hms/util/Encryptor.java
@@ -15,14 +15,17 @@ public class Encryptor {
 	*/
 	public static String encode(String word){
 		String RV = word.trim();
-		for(int i = 0; i< word.trim().length(); i++){
-			RV += " ";
-		}
 		char[] value = RV.toCharArray();
-		for(int i = 0; i<word.trim().length(); i++){
-			if(RV.charAt(i)*4+7 < Character.MAX_VALUE){
-				value[i] = (char) (RV.charAt(i)*4+7);
-				value[i+word.trim().length()] = '\t';
+		for(int i = 0; i<RV.length(); i++){
+			if(value[i] >= 'a' && value[i] <= 'z'){
+				value[i] -= 'a';
+				value[i] = (char)((value[i] + 13)%26);
+				value[i] += 'a';
+			}
+			if(value[i] >= 'A' && value[i] <= 'Z'){
+				value[i] -= 'A';
+				value[i] = (char)((value[i] + 13)%26);
+				value[i] += 'A';
 			}
 		}
 		
@@ -36,14 +39,6 @@ public class Encryptor {
 	*@return an an un-encoded string
 	*/
 	public static String decode(String word){
-		char[] RV = word.toCharArray();
-		int j = word.length()/2;
-		for(int i = 0; i< j; i++){
-			if(RV[i+j] == '\t')
-				RV[i] = (char) ((RV[i] - 7)/4);
-		}
-		String V = new String(RV);
-		return V.trim();
-		
+		return encode(word);		
 	}
 }
