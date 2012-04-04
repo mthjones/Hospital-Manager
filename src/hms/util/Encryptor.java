5df341d0a8b57aefc90fef163d4674dafd718eef
Matt Jones <m.t.h.jones@gmail.com>
2012-04-04 14:54:03 -0600

    Clean up Encryptor formatting

--- src/hms/util/Encryptor.java
+++ src/hms/util/Encryptor.java
@@ -1,47 +1,37 @@
 package hms.util;
 public class Encryptor {
-	
-	
-	public static void main(String[] args){
-		String str = "  This is a Test STREIFH^*%*	 	";
-		System.out.println(str.trim());
-		System.out.println(Encryptor.decode(Encryptor.encode(str)));
-		System.out.println(str.trim());
-		System.out.println(str.trim().equals(Encryptor.decode(Encryptor.encode(str))));
-	}
-	/*
-	*@param word the string to be encoded
-	*@return an encoded version of the word
-	*/
-	public static String encode(String word){
-		if(word == null || word.equals("null")){
+	/**
+	 * Encodes the given string.
+	 * @param word the string to be encoded
+	 * @return an encoded version of the given string
+	 */
+	public static String encode(String word) {
+		if(word == null || word.equals("null")) {
 			return "null"; 
 		}
-		String RV = word.trim();
-		char[] value = RV.toCharArray();
-		for(int i = 0; i<RV.length(); i++){
-			if(value[i] >= 'a' && value[i] <= 'z'){
+		String word = word.trim();
+		char[] value = word.toCharArray();
+		for (int i = 0; i < word.length(); i++) {
+			if (value[i] >= 'a' && value[i] <= 'z') {
 				value[i] -= 'a';
-				value[i] = (char)((value[i] + 13)%26);
+				value[i] = (char)((value[i] + 13) % 26);
 				value[i] += 'a';
 			}
-			if(value[i] >= 'A' && value[i] <= 'Z'){
+			if (value[i] >= 'A' && value[i] <= 'Z') {
 				value[i] -= 'A';
-				value[i] = (char)((value[i] + 13)%26);
+				value[i] = (char)((value[i] + 13) % 26);
 				value[i] += 'A';
 			}
 		}
-		
 		return new String(value);
-		
 	}
 	
-	
-	/*
-	*@param word an already encoded string
-	*@return an an un-encoded string
-	*/
-	public static String decode(String word){
+	/**
+	 * Decodes the given string.
+	 * @param word an already encoded string
+	 * @return a decoded version of the given string
+	 */
+	public static String decode(String word) {
 		return encode(word);		
 	}
 }
