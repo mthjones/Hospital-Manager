3a08ebeeae188534397153df107e02250cdccec3
Matt Jones <m.t.h.jones@gmail.com>
2012-04-04 15:10:03 -0600

    Add more tests to Encryptor

--- test/hms/util/EncryptorTest.java
+++ test/hms/util/EncryptorTest.java
@@ -1,21 +1,25 @@
 package hms.util;
 
-import static org.junit.Assert.*;
-
 import org.junit.*;
 import static org.junit.Assert.*;
 import hms.util.Encryptor;
 
-
 public class EncryptorTest {
-
 	@Test
-	public void test_encryptorTest() {
-		String str = "  This is a Test STREIFH^*%*	 	";
-//		System.out.println(str.trim());
-//		System.out.println(Encryptor.decode(Encryptor.encode(str)));
-//		System.out.println(str.trim());
-		assertTrue("Decrypted encrypted string should equal the original string trimmed", str.trim().equals(Encryptor.decode(Encryptor.encode(str))));
+	public void test_encodingTrimsWhitespaceFromBeginningAndEnd() {
+		String test = "     test string    ";
+		assertEquals(Encryptor.encode(test.trim()), Encryptor.encode(test));
 	}
-
+	
+	@Test
+	public void test_decodingTrimsWhitespaceFromBeginningAndEnd() {
+		String test = "     test string    ";
+		assertEquals(Encryptor.decode(test.trim()), Encryptor.decode(test));
+	}
+	
+	@Test
+	public void test_encodingAndThenDecodingReturnsTheSameValue() {
+		String test = "this is a test";
+		assertEquals(test, Encryptor.decode(Encryptor.encode(test)));
+	}
 }
