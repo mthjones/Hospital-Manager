7018944be06d103986f3cab547b659289e140d49
Matt Jones <m.t.h.jones@gmail.com>
2012-04-05 15:58:46 -0600

    Made the application a more standard size when started.

--- src/hms/views/ApplicationFrame.java
+++ src/hms/views/ApplicationFrame.java
@@ -5,7 +5,6 @@ import java.awt.*;
 
 public class ApplicationFrame extends JFrame {
 	private static final String TITLE = "Hospital Manager";
-	private static final Dimension INITIAL_SIZE = new Dimension(800, 600);
 	private static final Image ICON = new ImageIcon("/icon.png").getImage();
 	
 	public ApplicationFrame() {
@@ -32,7 +31,7 @@ public class ApplicationFrame extends JFrame {
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		pack();
 		setMinimumSize(new Dimension(getSize().width, 600));
-		setSize(getMinimumSize());
+		setSize(new Dimension(getMinimumSize().width, Math.min(getSize().height, Toolkit.getDefaultToolkit().getScreenSize().height)));
 		setLocationRelativeTo(null);
 	}
 }
