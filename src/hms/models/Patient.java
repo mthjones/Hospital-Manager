95f089f2bffaa7e345edb026f5bacb976bcb042c
Matt Jones <m.t.h.jones@gmail.com>
2012-04-05 16:15:07 -0600

    Remove unnecessary methods and change public members to private in Patient

--- src/hms/models/Patient.java
+++ src/hms/models/Patient.java
@@ -1,42 +1,34 @@
 package hms.models;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.sql.SQLException;
 import java.sql.ResultSet;
-import java.util.Date;
-import java.util.Vector;
 
-import hms.util.Database;
-import hms.util.Priority;
 import hms.util.*;
 
 public class Patient {
-	//TODO create getters and setters
-	public String healthcare_number;
-	public String name;
-	public String phone_number;
-	public String email;
+	private String healthcare_number;
+	private String name;
+	private String phone_number;
+	private String email;
 	private String gender;
 	private String treatment;
-	public String address;
+	private String address;
 	private Date birthdate;
-	public String medications;
-	public String special_care;
-	public String history;
-	public String comments;
-	public String emerg_name;
-	public String emerg_phone_number;
-	public String emerg_email;
+	private String medications;
+	private String special_care;
+	private String history;
+	private String comments;
+	private String emerg_name;
+	private String emerg_phone_number;
+	private String emerg_email;
 	private boolean in_hospital;
-	public int ward_id;
-	public int room_id;
-	public int bed_id;
+	private int ward_id;
+	private int room_id;
+	private int bed_id;
 	private Priority priority;
-	public ArrayList<String> errors = new ArrayList<String>();
-	public static final String RESERVED_TEST_NAME = "Forbidden Name";
-
-	public Patient()
-	{}
+	private ArrayList<String> errors = new ArrayList<String>();
+	static final String RESERVED_TEST_NAME = "Forbidden Name";
 
 	public Patient(String healthcare_number, 
 			String name, 
@@ -156,27 +148,6 @@ public class Patient {
 		return patients;
 	}
 
-	// /**
-	//  * Finds all patients and returns them as in a patient objects. If
-	//  * no patients exist, null is returnes
-	//  * @return Patients if found, or null if there arent any
-	//  */
-	// public static Vector<Patient> findAllPatients() throws SQLException{
-	// 	ResultSet patient = Database.getInstance().executeQuery("SELECT * FROM patient");
-	// 	if (patient == null) return null;
-	// 	patient.first();
-	// 	Vector<Patient> paitents = new Vector<Patient>();
-	// 	while(!patient.isLast()){
-	// 		paitents.add(new Patient(patient.getString(1), patient.getString(2), patient.getString(3),
-	//                                     patient.getString(4), patient.getString(5), patient.getString(6), patient.getString(7),
-	//                                     patient.getDate(8), patient.getString(9), patient.getString(10), patient.getString(11),
-	//                                     patient.getString(12), patient.getString(13), patient.getString(14), patient.getString(15),
-	//                                     patient.getBoolean(16), patient.getInt(17), patient.getInt(18), patient.getInt(19)) );
-	// 		if(!patient.next()) return null;
-	// 	}
-	// 	return paitents;
-	// }
-
 	/**
 	 * Tries to save the patient to the database. Returns true on a successful save or false
 	 * if the save fails for any reason.
@@ -232,25 +203,8 @@ public class Patient {
 			return false;
 		}
 	}
-
-	/**
-	 * Tries to delete a patient with a corresponding healthcare number from the database.
-	 * Static version of delete() for convenience's sake.
-	 * @return true if the delete is successful; false otherwise
-	 */
-	public static boolean deleteFromString(String healthcare_number) {
-		try {
-			int patient = Database.getInstance().executeUpdate("DELETE FROM patient WHERE healthcare_number = '" + healthcare_number + "'");
-			if (patient == 0) {
-				return false;
-			}
-			return true;
-		} catch (SQLException sqle) {
-			return false;
-		}
-	}
 	
-		public String getHealthcareNumber() {
+	public String getHealthcareNumber() {
 		return this.healthcare_number;
 	}
 	
