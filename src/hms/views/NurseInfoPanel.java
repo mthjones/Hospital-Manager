a1e2f4c84c7010c0955b912a8958d4cb51f4f88f
Matt Jones <m.t.h.jones@gmail.com>
2012-04-05 23:36:19 -0600

    Implementier nurse creation, editing and validation

--- src/hms/views/NurseInfoPanel.java
+++ src/hms/views/NurseInfoPanel.java
@@ -1,6 +1,8 @@
 package hms.views;
 
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.text.*;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -31,6 +33,8 @@ public class NurseInfoPanel extends AbstractInfoPanel {
 	private JLabel wardLabel = new JLabel("Ward:");
 	private JLabel passwordLabel = new JLabel("Password:");
 	private JLabel addressLabel = new JLabel("Address:");
+	
+	private Integer id_number;
 	
 	public NurseInfoPanel() {
 		initUI();
@@ -78,22 +82,87 @@ public class NurseInfoPanel extends AbstractInfoPanel {
 		addressField.setRows(3);
 		this.add(addressField, "span 2, wrap");
 		
+		requiredComponents.add(nameLabel);
+		requiredComponents.add(phoneLabel);
+		requiredComponents.add(salaryLabel);
+		requiredComponents.add(sinLabel);
+		
 		setTextComponentBorders();
 	}
 	
 	public void reset() {
-		
+		for (Component comp : getComponents()) {
+			if (comp instanceof JTextComponent) {
+				JTextComponent textComp = (JTextComponent)comp;
+				textComp.setText("");
+				editableComponents.add((JTextComponent)comp);
+			}
+		}
+		maleButton.setSelected(false);
+		femaleButton.setSelected(false);
+		wardsDropdown.setSelectedIndex(0);
 	}
 	
 	public boolean validateInformation() {
-		return true;
+		boolean validated = true;
+		
+		if (nameField.getText().equals("")) {
+			nameLabel.setForeground(Color.RED);
+			validated = false;
+		}
+		
+		if (sinField.getText().equals("")) {
+			sinLabel.setForeground(Color.RED);
+			validated = false;
+		}
+		
+		if (phoneField.getText().equals("")) {
+			phoneLabel.setForeground(Color.RED);
+			validated = false;
+		}
+		
+		try {
+			Integer.parseInt(salaryField.getText());
+		} catch (NumberFormatException e) {
+			salaryLabel.setForeground(Color.RED);
+			validated = false;
+		}
+		
+		return validated;
 	}
 	
 	public void loadInformation(Object objToLoad) {
 		Nurse nurse = (Nurse)objToLoad;
+		nameField.setText(nurse.getName());
+		phoneField.setText(nurse.getPhoneNumber());
+		pagerField.setText(nurse.getPagerNumber());
+		emailField.setText(nurse.getEmail());
+		addressField.setText(nurse.getAddress());
+		sinField.setText(nurse.getSIN());
+		salaryField.setText(Integer.toString(nurse.getSalary()));
+		passwordField.setText(nurse.getPassword());
+		wardsDropdown.setSelectedItem(nurse.getWard());
+		if (nurse.getGender().equals("M")) {
+			maleButton.setSelected(true);
+			femaleButton.setSelected(false);
+		} else {
+			maleButton.setSelected(false);
+			femaleButton.setSelected(true);
+		}
+		id_number = nurse.getID();
 	}
 	
 	public AbstractModel modelFromInformation() {
-		return null;
+		return new Nurse(nameField.getText(),
+						 phoneField.getText(),
+						 pagerField.getText(),
+						 emailField.getText(),
+						 addressField.getText(),
+						 sinField.getText(),
+						 (id_number == null) ? Nurse.generateIDNumber() : id_number,
+						 maleButton.isSelected() ? "M" : "F",
+						 Integer.parseInt(salaryField.getText()),
+						 wardsDropdown.getSelectedIndex(),
+						 new String(passwordField.getPassword()));
 	}
 }
