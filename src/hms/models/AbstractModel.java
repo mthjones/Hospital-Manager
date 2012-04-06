b913ae3e961fbe6e8b976db0c10aeeb9a7f44cfd
Matt Jones <m.t.h.jones@gmail.com>
2012-04-05 20:28:28 -0600

    AbstractModel now has some basic documentation

--- src/hms/models/AbstractModel.java
+++ src/hms/models/AbstractModel.java
@@ -3,7 +3,15 @@ package hms.models;
 import java.sql.SQLException;
 
 public interface AbstractModel {
+	/**
+	 * Should delete the object's representation in the database.
+	 * @throws SQLException If there is an SQL error
+	 */
 	public boolean delete() throws SQLException;
 	
+	/**
+	 * Should create the object's representation in the database.
+	 * @throws SQLException If there is an SQL error
+	 */
 	public boolean create() throws SQLException;
 }
