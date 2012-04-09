5b58774351bc9ba3985eb8ac0061658445b0859e
Matt Jones <m.t.h.jones@gmail.com>
2012-04-09 14:01:03 -0600

    Nurse is now tested. (all passing)

--- test/hms/models/NurseTest.java
+++ test/hms/models/NurseTest.java
@@ -1,80 +1,124 @@
 package hms.models;
 
+import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import hms.util.*;
 
 public class NurseTest {
-
-	/*@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
+	private Nurse nurse;
+	private int nurseID;
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setup() {
+		this.nurseID = Nurse.generateIDNumber();
+		this.nurse = new Nurse("nurse1", "555-555-5555", "123-456-7890", "Jane@nurses.com", "123, 4th avenue", "123-456-789", nurseID, "F", 50000, 0, "");
 	}
-
+	
 	@After
-	public void tearDown() throws Exception {
-	}*/
+	public void teardown() {
+		// Just in case a nurse.delete() statement was forgotten
+		try {
+			this.nurse.delete();
+		} catch (SQLException sqle) { }
+	}
 
 	@Test
-	public void test_createNurse() throws SQLException{
-		Nurse nurse = new Nurse("nurse1", "555-555-5555", "123-456-7890", "Jane@nurses.com", "123, 4th avenue", "123-456-789", 1234, "f", 50000, 0,"");
-		assertTrue("Nurse should be created", nurse.create());
-		nurse.delete();
-	}
-	
-	@Test
-	public void test_cannotCreateNurseWithSameIDNumber() throws SQLException{
-		Nurse nurse1 = new Nurse("nurse2", "555-555-5555", "123-456-7890", "Jane@nurses.com", "123, 4th avenue", "123-456-789", 1234, "f", 50000, 0,"");
-		assertTrue("Nurse should be created", nurse1.create());
-		Nurse nurse2 = new Nurse("nurse3", "555-555-5555", "123-456-7890", "John@nurses.com", "123, 4th avenue", "123-456-789", 1234, "m", 50000, 0,"");
-		assertFalse("Duplicate nurse should not be created", nurse2.create());
-		nurse1.delete();
-	}
-	
-	@Test
-	public void test_deleteNurse() {
+	public void createNurse() {
 		try {
-			Nurse nurse = new Nurse("nurse1", "555-555-5555", "123-456-7890", "Jane@nurses.com", "123, 4th avenue", "123-456-789", 1111, "f", 50000, 0,"");
+			ResultSet nurseResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM nurse WHERE id_number = " + nurseID);
+			nurseResults.next();
+			int previousWithID = nurseResults.getInt(1);
+			
+			assertTrue("Nurse should be created", nurse.create());
+			
+			nurseResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM nurse WHERE id_number = " + nurseID);
+			nurseResults.next();
+			assertEquals(previousWithID + 1, nurseResults.getInt(1));
+			
+			nurse.delete();
+		} catch (SQLException sqle) { }
+	}
+	
+	@Test
+	public void cannotCreateNurseWithSameIDNumber() {
+		try {
+			assertTrue("Nurse should be created", nurse.create());
+			
+			ResultSet nurseResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM nurse WHERE id_number = " + nurseID);
+			nurseResults.next();
+			int previousWithID = nurseResults.getInt(1);
+			
+			Nurse nurse2 = new Nurse("nurse3", "555-555-5555", "123-456-7890", "John@nurses.com", "123, 4th avenue", "123-456-789", nurseID, "M", 50000, 0,"");
+			assertFalse("Duplicate nurse should not be created", nurse2.create());
+			
+			nurseResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM nurse WHERE id_number = " + nurseID);
+			nurseResults.next();
+			assertEquals(previousWithID, nurseResults.getInt(1));
+			
+			nurse.delete();
+		} catch (SQLException sqle) { }
+	}
+	
+	@Test
+	public void deleteNurse() {
+		try {
 			nurse.create();
+			
+			ResultSet nurseResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM nurse WHERE id_number = " + nurseID);
+			nurseResults.next();
+			int previousWithID = nurseResults.getInt(1);
+			
 			assertTrue(nurse.delete());
+			
+			nurseResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM nurse WHERE id_number = " + nurseID);
+			nurseResults.next();
+			assertEquals(previousWithID - 1, nurseResults.getInt(1));
 		} catch(SQLException e) {
 			fail("SQLException occurred.");
 		}
 	}
 	
 	@Test
-	public void test_cannotDeleteNonexistantNurse(){
+	public void nonexistantNurseDeletionHasNoEffect() {
 		try{
-			Nurse nurse = new Nurse("Jane Doe", "123-456-7890", "555-5555", "test@test.com", "123, 4th avenue", "123-456-7890", 2222, "f", 100000, 0,"");
+			ResultSet nurseResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM nurse");
+			nurseResults.next();
+			int previousWithID = nurseResults.getInt(1);
+			
 			assertFalse(nurse.delete());
+			
+			nurseResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM nurse");
+			nurseResults.next();
+			assertEquals(previousWithID, nurseResults.getInt(1));
 		} catch(SQLException e) {
 			fail("SQLException occured.");
 		}
 	}
 	
 	@Test
-	public void test_findNurse() {
+	public void findNurse() {
 		try{
-			Nurse nurse = new Nurse("Jane Doe", "123-456-7890", "555-5555", "test@test.com", "123, 4th avenue", "123-456-7890", 2222, "f", 100000, 0,"");
+			assertNull(Nurse.find(nurseID));
 			nurse.create();
-			assertNotNull(Nurse.find(2222));
+			assertNotNull(Nurse.find(nurseID));
 			nurse.delete();
 		} catch(SQLException e) {
 			fail("SQLException occurred.");
 		}
 	}
-
+	
+	@Test
+	public void idGeneratorProducesUniqueResult() {
+		int new_id = Nurse.generateIDNumber();
+		try {
+			ResultSet nurseResults = Database.getInstance().executeQuery("SELECT id_number FROM nurse");
+			while (nurseResults.next()) {
+				assertFalse(new_id == nurseResults.getInt("id_number"));
+			}
+		} catch (SQLException sqle) {}
+	}
 }
