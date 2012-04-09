ab0a8cbba9b56ccc0c92fe5d427e96f3e5b1c3a9
Matt Jones <m.t.h.jones@gmail.com>
2012-04-09 14:17:47 -0600

    User is now tested. (all passing)

--- test/hms/models/UserTest.java
+++ test/hms/models/UserTest.java
@@ -1,52 +1,110 @@
 package hms.models;
 
-import java.sql.SQLException;
-import java.util.Arrays;
-
-import hms.models.User;
-
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import hms.util.*;
+
 public class UserTest {
+	private User user;
+	private String username;
+	private String password;
+	
+	@Before
+	public void setup() {
+		this.username = "testuser";
+		this.password = "testpass";
+		this.user = new User(username, password);
+	}
+	
+	@After
+	public void teardown() {
+		try {
+			this.user.delete();
+		} catch (SQLException sqle) { }
+	}
+	
 	@Test 
-	public void test_createUser() throws SQLException {
-		User user = new User("testuser", "testpass");
-		user.toString();
-		assertTrue(user.create());
-		user.delete();
+	public void createUser() {
+		try {
+			ResultSet userResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM user WHERE username = " + username);
+			userResults.next();
+			int previousWithUsername = userResults.getInt(1);
+			
+			assertTrue(user.create());
+			
+			userResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM user WHERE username = " + username);
+			userResults.next();
+			assertEquals(previousWithUsername + 1, userResults.getInt(1));
+			
+			user.delete();
+		} catch (SQLException sqle) {}
 	}
 	
 	@Test
-	public void test_cannotCreateUserWithSameUsername() throws SQLException {
-		User user = new User("testuser", "testpass");
-		user.create();
-		User user2 = new User("testuser", "testpass2");
-		assertFalse(user2.create());
-		user.delete();
+	public void cannotCreateUserWithSameUsername() {
+		try {
+			user.create();
+			
+			ResultSet userResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM user WHERE username = " + username);
+			userResults.next();
+			int previousWithUsername = userResults.getInt(1);
+			
+			User user2 = new User("testuser", "testpass2");
+			assertFalse(user2.create());
+			
+			userResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM user WHERE username = " + username);
+			userResults.next();
+			assertEquals(previousWithUsername, userResults.getInt(1));
+			
+			user.delete();
+		} catch (SQLException sqle) {}
 	}
 	
 	@Test
-	public void test_deleteUser() throws SQLException {
-		User user = new User("testuser", "testpass");
-		user.create();
-		assertTrue(user.delete());
+	public void deleteUser() {
+		try {
+			user.create();
+			
+			ResultSet userResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM user WHERE username = " + username);
+			userResults.next();
+			int previousWithUsername = userResults.getInt(1);
+			
+			assertTrue(user.delete());
+			
+			userResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM user WHERE username = " + username);
+			userResults.next();
+			assertEquals(previousWithUsername - 1, userResults.getInt(1));
+		} catch (SQLException sqle) {}
 	}
 	
 	@Test
-	public void test_cannotDeleteNonexistantUser() throws SQLException {
-		User user = new User("testuser", "testpass");
-		assertFalse(user.delete());
+	public void nonexistantUserDeletionHasNoEffect() {
+		try {
+			ResultSet userResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM user");
+			userResults.next();
+			int previousWithUsername = userResults.getInt(1);
+			
+			assertFalse(user.delete());
+			
+			userResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM user");
+			userResults.next();
+			assertEquals(previousWithUsername, userResults.getInt(1));
+		} catch (SQLException sqle) {}
 	}
 	
 	@Test
-	public void test_authenticateUser() throws SQLException {
-		User user = new User("testuser2", "testpass");
-		user.create();
-		assertTrue("right user, right pass", User.authenticate("testuser2", "testpass"));
-		assertFalse("wrong user, right pass", User.authenticate("wronguser", "testpass"));
-		assertFalse("right user, wrong pass", User.authenticate("testuser2", "wrongpass"));
-		assertFalse("wrong user, wrong pass", User.authenticate("wronguser", "wrongpass"));
-		user.delete();
+	public void authenticateUser() {
+		try {
+			user.create();
+			assertTrue("right user, right pass", User.authenticate("testuser", "testpass"));
+			assertFalse("wrong user, right pass", User.authenticate("wronguser", "testpass"));
+			assertFalse("right user, wrong pass", User.authenticate("testuser", "wrongpass"));
+			assertFalse("wrong user, wrong pass", User.authenticate("wronguser", "wrongpass"));
+			user.delete();
+		} catch (SQLException sqle) {}
 	}
 }
