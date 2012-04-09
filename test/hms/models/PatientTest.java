c19d817ea66a6b0e0e42e17e1d35f3ddf79a3664
Matt Jones <m.t.h.jones@gmail.com>
2012-04-09 13:44:42 -0600

    Patient is now tested. (all passing)

--- test/hms/models/PatientTest.java
+++ test/hms/models/PatientTest.java
@@ -1,124 +1,122 @@
 package hms.models;
 
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.util.Date;
 
-import hms.models.Patient;
 import hms.util.*;
 
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class PatientTest {
-	// @Test
-	// public void test_createPatient() throws SQLException {
-	// 	Patient patient = new Patient("123456789", 
-	// 								  "John Q Public", 
-	// 								  "123456789", 
-	// 								  "john@example.org", 
-	// 								  "M", 
-	// 								  "Test Treatment", 
-	// 								  "123 Abc Street", 
-	// 								  new Date(), 
-	// 								  "", 
-	// 								  "", 
-	// 								  "", 
-	// 								  "", 
-	// 								  "", 
-	// 								  "", 
-	// 								  "",
-	// 								  true,
-	// 								  1,
-	// 								  1,
-	// 								  1,
-	// 								  Priority.HIGH);
-	// 	assertTrue(patient.create());
-	// 	patient.delete();
-	// }
+	private Patient patient;
+	private String patientID;
 	
-	// @Test
-	// public void test_cannotCreatePatientWithSameHealthcareNumber() throws SQLException {
-	// 	Patient patient = new Patient("123456789", 
-	// 								  "John Q Public", 
-	// 								  "0123456789", 
-	// 								  "john@example.org", 
-	// 								  "M", 
-	// 								  "Test Treatment", 
-	// 								  "123 Abc Street", 
-	// 								  new Date(), 
-	// 								  "", 
-	// 								  "", 
-	// 								  "", 
-	// 								  "", 
-	// 								  "", 
-	// 								  "", 
-	// 								  "",
-	// 								  true,
-	// 								  1,
-	// 								  1,
-	// 								  1,
-	// 								  Priority.HIGH);
-	// 	assertTrue(patient.create());
-	// 	Patient patient2 = new Patient("123456789", "Mary Sue", "9876543210", "mary@example.org", "M", "Test Treatment", "123 Abc Street", new Date(), "", "", "", "", "", "", "", true,1,1,1, Priority.HIGH);
-	// 	boolean hadException = false;
-	// 	try{
-	// 	assertFalse(patient2.create());
-	// 	}catch(Exception e){
-	// 		hadException = true;
-	// 	}
-	// 	assertTrue("Exception should have been thrown", hadException);
-	// 	patient.delete();
-	// }
+	@Before
+	public void setup() {
+		this.patientID = "2187312798";
+		this.patient = new Patient(patientID, 
+								   "John Q Public", 
+								   "0123456789", 
+								   "john@example.org", 
+								   "M", 
+								   "Test Treatment", 
+								   "123 Abc Street", 
+								   new Date(), 
+								   "", 
+								   "", 
+								   "", 
+								   "", 
+								   "", 
+								   "", 
+								   "",
+								   true,
+								   Ward.find(1),
+								   Room.find(3),
+								   Bed.find(12),
+								   Priority.HIGH);
+	}
 	
-	// @Test
-	// public void test_deletePatient() throws SQLException {
-	// 	Patient patient = new Patient("123456789", "John Q Public", "123456789", "john@example.org", "M", "Test Treatment", "123 Abc Street", new Date(), "", "", "", "", "", "", "", true,1,1,1, Priority.HIGH);
-	// 	patient.create();
-	// 	assertTrue(patient.delete());
-	// }
+	@After
+	public void teardown() {
+		// Just in case a patient.delete() statement was forgotten
+		try {
+			this.patient.delete();
+		} catch (SQLException sqle) { }
+	}
 	
-	// @Test
-	// public void test_cannotDeleteNonexistantPatient() throws SQLException {
-	// 	Patient patient = new Patient("123456789", "John Q Public", "123456789", "john@example.org", "M", "Test Treatment", "123 Abc Street", new Date(), "", "", "", "", "", "", "", true,1,1,1, Priority.HIGH);
-	// 	assertFalse(patient.delete());
-	// }
+	@Test
+	public void createPatient() {
+		try {
+			ResultSet patientResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM patient WHERE healthcare_number = " + patientID);
+			patientResults.next();
+			int previousWithHealthNum = patientResults.getInt(1);
+			
+			assertTrue(patient.create());
+			
+			patientResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM patient WHERE healthcare_number = " + patientID);
+			patientResults.next();
+			assertEquals(previousWithHealthNum + 1, patientResults.getInt(1));
+			
+			patient.delete();
+		} catch (SQLException sqle) { }
+	}
 	
-	// @Test
-	// public void test_findPatient() throws SQLException {
-	// 	Patient patient = new Patient("123456789", "John Q Public", "123456789", "john@example.org", "M", "Test Treatment", "123 Abc Street", new Date(), "", "", "", "", "", "", "", true,1,1,1, Priority.HIGH);
-	// 	patient.create();
-	// 	assertNotNull(Patient.find("123456789"));
-	// 	patient.delete();
-	// }
+	@Test
+	public void deletePatient() {
+		try {
+			patient.create();
+			
+			ResultSet patientResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM patient WHERE healthcare_number = " + patientID);
+			patientResults.next();
+			int previousWithHealthNum = patientResults.getInt(1);
+			
+			assertTrue(patient.delete());
+			
+			patientResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM patient WHERE healthcare_number = " + patientID);
+			patientResults.next();
+			assertEquals(previousWithHealthNum - 1, patientResults.getInt(1));
+		} catch (SQLException sqle) { }
+	}
 	
-	// @Test
-	// public void test_serchForPatients_patientDoesntExist() throws SQLException{
-	// 	assertNull(Patient.findByName(Patient.RESERVED_TEST_NAME));
-	// }
+	@Test
+	public void patientBedAvailabilityChangedOnCreationAndDeletion() {
+		try {
+			assertFalse(patient.getBed().getOccupied());
+			
+			patient.create();
+			
+			assertTrue(patient.getBed().getOccupied());
+			
+			patient.delete();
+			
+			assertFalse(patient.getBed().getOccupied());
+		} catch (SQLException sqle) { }
+	}
 	
-	// @Test
-	// public void test_serchForPatients_oneValidPatientExists() throws SQLException{
-	// 	Patient patient = new Patient("123456789", "John Q Public", "123456789", "john@example.org", "M", "Test Treatment", "123 Abc Street", new Date(), "", "", "", "", "", "", "", true,1,1,1, Priority.HIGH);
-	// 	patient.create();
-	// 	Patient [] results = Patient.findByName("John Q Public");
-	// 	assertNotNull(results);
-	// 	assertEquals(1, results.length);
-	// 	assertTrue(results[0].getName().equals("John Q Public"));
-	// 	patient.delete();
-	// }
+	@Test
+	public void nonexistantPatientDeletionHasNoEffect() {
+		try {
+			ResultSet patientResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM patient");
+			patientResults.next();
+			int rowCount = patientResults.getInt(1);
+			
+			assertFalse(patient.delete());
+			
+			patientResults = Database.getInstance().executeQuery("SELECT COUNT(*) FROM patient");
+			patientResults.next();
+			assertEquals(rowCount, patientResults.getInt(1));
+		} catch (SQLException sqle) { }
+	}
 	
-	// @Test
-	// public void test_serchForPatients_twoValidPatientsExist() throws SQLException{
-	// 	Patient patient1 = new Patient("123456789", "John Q Public", "123456789", "john@example.org", "M", "Test Treatment", "123 Abc Street", new Date(), "", "", "", "", "", "", "", true,1,1,1, Priority.HIGH);
-	// 	Patient patient2 = new Patient("987654321", "John Q Public", "123456789", "john@example.org", "M", "Test Treatment", "123 Abc Street", new Date(), "", "", "", "", "", "", "", true,1,1,1, Priority.HIGH);
-	// 	patient1.create();
-	// 	patient2.create();
-	// 	Patient [] results = Patient.findByName("John Q Public");
-	// 	assertNotNull(results);
-	// 	assertEquals(2, results.length);
-	// 	assertTrue(results[0].getName().equals("John Q Public") && results[0].getHealthcareNumber().equals("123456789"));
-	// 	assertTrue(results[1].getName().equals("John Q Public") && results[1].getHealthcareNumber().equals("987654321"));
-	// 	patient1.delete();
-	// 	patient2.delete();
-	// }
+	@Test
+	public void findPatient() {
+		try {
+			assertNull(Patient.find(patientID));
+			patient.create();
+			assertNotNull(Patient.find(patientID));
+			patient.delete();
+		} catch (SQLException sqle) { }
+	}
 }
